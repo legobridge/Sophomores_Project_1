c4a78a2171638ede0b86f5fd44c6a58787df5d7b
Kushal-sophos <f2015289@hyderabad.bits-pilani.ac.in>
2017-03-07 00:50:11 +0530

    Landing Page

--- public/index.php
+++ public/index.php
@@ -2,5 +2,14 @@
 
     // configuration
     require("../includes/config.php"); 
+    
+    if ($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+    	render("landing.php", ["title" => "Home"]);
+    }
 
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+   	{
+   		
+   	}
 ?>
---
+++ views/landing.php
@@ -0,0 +1,11 @@
+<form action = "index.php" method = "post">
+	<div class = "form-group">
+		<input type = "text" name = "link">
+	</div>
+	<div class = "form-group">
+        <button class = "btn btn-default" type = "submit">
+            <span aria-hidden = "true" class = "glyphicon glyphicon-education"></span>
+            Scrape
+        </button>
+    </div>
+</form>

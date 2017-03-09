a26dca7438eb4d602019b59a44ae0844a969910d
Kushal-sophos <f2015289@hyderabad.bits-pilani.ac.in>
2017-03-10 00:56:29 +0530

    Remove Old Style PHP Model from index.php

--- public/index.php
+++ public/index.php
@@ -1,23 +1,7 @@
 <?php
 
     // configuration
-    require("../includes/config.php"); 
-    
-    if ($_SERVER["REQUEST_METHOD"] == "GET")
-    {
-    	render("landing.php", ["title" => "Home"]);
-    }
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST")
-   	{
-   		$link = $_POST["link"];
-   		if (empty($link))
-      {
-          apologize("You must enter a link.");
-      }
-      else
-      {
-      	// Start Scraping
-      }
-   	}
+    require("../includes/config.php");
+    // see render function in helpers.php
+    render("landing.php", ["title" => "Home"]);
 ?>

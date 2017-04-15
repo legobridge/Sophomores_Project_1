b4288c9f2139af826325640dca5e940a8adf5dc3
Kushal-sophos <f2015289@hyderabad.bits-pilani.ac.in>
2017-04-15 15:22:23 +0000

    Adjust for CS50 IDE

--- views/header.php
+++ views/header.php
@@ -7,16 +7,16 @@
 	    <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
 	    <meta name = "viewport" content = "width = device-width, initial-scale = 1">
 	    <!-- Bootstrap -->
-	    <link href = "../public/css/bootstrap.min.css" rel = "stylesheet">
+	    <link href = "/css/bootstrap.min.css" rel = "stylesheet">
 	    <!-- Stylesheet -->
-	    <link href = "../public/css/styles.css" rel = "stylesheet">
+	    <link href = "/css/styles.css" rel = "stylesheet">
 
 	    <!-- jQuery -->
-	    <script src = "../public/js/jquery.min.js"></script>
+	    <script src = "/js/jquery.min.js"></script>
 	    <!-- Bootstrap JS -->
-	    <script src = "../public/js/bootstrap.min.js"></script>
+	    <script src = "/js/bootstrap.min.js"></script>
         <!-- Custom Scripts -->
-        <script src = "../public/js/scripts.js"></script>
+        <script src = "/js/scripts.js"></script>
 
         <?php if (isset($title)): ?>
             <title> Scrapesha : <?= htmlspecialchars($title) ?></title>
@@ -31,5 +31,5 @@
         <div class = "container">
 
             <div id = "top">
-                <a href="/"><img id = "logo" alt = "Scrapesha" src = "../public/img/scrapesha_logo.png"/></a>
+                <a href="/"><img id = "logo" alt = "Scrapesha" src = "/img/scrapesha_logo.png"/></a>
             <div id="middle">
